3b59f856e5e62e22935ea9b494ccf2e41b545b1b
Bethsheba Zebata <bethzebata@gmail.com>
2020-09-12 23:32:32 -0500

    working on test katas assessment

--- tests/test_katas.py
+++ tests/test_katas.py
@@ -4,16 +4,22 @@ import katas
 
 class TestKatas(unittest.TestCase):
     def test_add(self):
-        self.fail("TODO: Write add unit test")
+        result = katas.add(10, 5)
+        self.assertEqual(result, 15)
 
     def test_multiply(self):
-        self.fail("TODO: Write multiply unit test")
+        result = katas.multiply(10, 5)
+        self.assertEqual(result, 50)
 
     def test_power(self):
-        self.fail("TODO: Write power unit test")
+        result = katas.power(2, 2)
+        self.assertEqual(result, 4)
 
     def test_factorial(self):
-        self.fail("TODO: Write factorial unit test")
+        x = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800,
+             479001600, 6227020800, 87178291200, 1307674368000]
+        for i in x():
+            self.assertEqual(katas.factorial(i), x)
 
     def test_fibonacci(self):
         self.fail("TODO: Write fibonacci unit test")
